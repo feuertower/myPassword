63136b803c5c2736d262e915c19b7ef3d9091de3
home <do.scheible@googlemail.com>
2019-08-19 21:31:29 +0200

    implement layout of add dialog, buttons arent displayed

--- app/src/main/java/com/android/mypassword/addPassword.java
+++ app/src/main/java/com/android/mypassword/addPassword.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class addPassword extends Dialog implements
 android.view.View.OnClickListener{
 
     public Context context;
+    public Button addBtn, cancelBtn;
 
     public addPassword(Context c)
     {
@@ -23,13 +25,28 @@ android.view.View.OnClickListener{
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.content_add_password);
+
+        addBtn = (Button)findViewById(R.id.addBtn);
+        addBtn.setOnClickListener( this );
+        cancelBtn = (Button)findViewById(R.id.cancelBtn);
+        cancelBtn.setOnClickListener( this );
+
     }
 
 
     @Override
     public void onClick(View view)
     {
+        switch ( view.getId() )
+        {
+            case R.id.addBtn:
+                // add entry
+                break;
 
+            case R.id.cancelBtn:
+                // cancel dialog
+                break;
+        }
     }
 
 
